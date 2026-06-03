84b4cf5fad90492383b831a642aa630b41d7eb48
sardorchik056-a11y <sardorchik056@gmail.com>
2026-06-03 06:07:56 +0300

    Update hunt.py

--- hunt.py
+++ hunt.py
@@ -892,7 +892,7 @@ def sword_detail_keyboard(data: dict, sword_key: str) -> InlineKeyboardMarkup:
 
         if not owned:
             builder.row(InlineKeyboardButton(
-                text=f'{_fmt(sword["price"])} {COIN}',
+                text=f'{_fmt(sword["price"])}',
                 callback_data=f'sword_buy_{sword_key}',
                 icon_custom_emoji_id=_E["coin"]
             ))
